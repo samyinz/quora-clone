6c3130340318e62a42fb963fe308da53b3018973
lwynwai <wynwai11@gmail.com>
2017-02-13 16:14:00 +0800

    initialize commit

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -3,14 +3,4 @@ get '/' do
 end
 
 
-post '/signup' do
-	@user = User.new(params[:user])
-	if @user.save
-		#what should happen if the user is saved?
-		@message = "Thank you for signing up"
-	else
-		# what should happen if the user keyed in invalid date?
-		@message = "Please type in a valid email"
-	end
-	redirect '/'
-end
+
